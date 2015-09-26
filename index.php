70d5015c9dab37f163dec7052e69971a0232225a
Todd Beckman <toddbeckman1@gmail.com>
2015-09-25 22:19:29 -0600

    added a carousel

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
         <link rel="stylesheet" type="text/css"
             href="/css/style.css">
         <script type="text/javascript" src="/js/script.js"></script>
+        <script type="text/javascript" src="/includes/jquery.min.js"></script>
+        <script type="text/javascript" src="/includes/bootstrap.min.js"></script>
     </head>
-    <body onload="loadlinks()">
+    <body>
         <div id="wrapper" class="container-fluid">
             <div class="row">
                 <div id="header" class="col-xs-12 col-sm-8">
@@ -42,9 +44,41 @@
                 </a>
             </div>
             <div class="row">
-                <div id="intro" class="col-xs-12 text-center">
-                    <p>Integrity, Synergy, Scalability</p>
-                </div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="/img/alex.jpg" alt="Alex">
+        </div>
+        <div class="item">
+            <img src="/img/todd.jpg" alt="Todd">
+        </div>
+        <div class="item">
+            <img src="/img/kalvyn.jpg" alt="Kalvyn">
+        </div>
+        <div class="item">
+            <img src="/img/luke.jpg" alt="Luke">
+        </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
             </div>
             <div class="row">
                 <div id="desc" class="col-xs-12">
